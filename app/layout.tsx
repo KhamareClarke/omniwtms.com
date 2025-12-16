c1028fcdb63ce401a8349e168914a64f05190f47
Khamare Clarke <khamareclarke@gmail.com>
2025-12-16 00:10:44 +0000

    Update favicon to use OmniWTMS logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,10 +33,10 @@ export default function RootLayout({
         <link rel="canonical" href="https://omniwtms.com" />
         
         {/* Favicon */}
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="any" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/images/omniwtmslogo.png" type="image/png" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/omniwtmslogo.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/omniwtmslogo.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/omniwtmslogo.png" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="msapplication-TileColor" content="#2563eb" />
         <meta name="theme-color" content="#2563eb" />
